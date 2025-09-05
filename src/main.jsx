5b344ce7a35b941bda8698f61b1289a790d77f1f
Felipe Rodrigues <felipe.rodri.pessoal@gmail.com>
2025-09-04 22:46:43 -0300

    Ajustes finais

--- src/main.jsx
+++ src/main.jsx
@@ -8,24 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-</head>
-<body>
-  <h1>Seja bem-vindo ao EcoEssencia 🌱</h1>
-<h4>Nosso e-commerce foi desenvolvido com um objetivo: promover um consumo mais consciente e acessível para todos. Nosso foco é em produtos sustentáveis e ecológicos, criteriosamente escolhidos para minimizar o impacto ambiental e incentivar um estilo de vida mais saudável e equilibrado.</h4>
-
-<h4>Em nosso catálogo, oferecemos desde produtos cotidianos, como utensílios reutilizáveis, cosméticos naturais e roupas sustentáveis, até itens para um lar sustentável, todos criados com compromisso social e ambiental.</h4>
-
-<h4>Acreditamos que pequenas decisões podem causar grandes impactos. Portanto, colaboramos exclusivamente com fornecedores que se dedicam a práticas éticas, materiais sustentáveis e processos de produção com baixo impacto ambiental.</h4>
-
-<h4>Aqui, você não adquire apenas produtos, mas também tem a oportunidade de contribuir para um futuro mais sustentável, equitativo e consciente.</h4>
-
-<h4>🌍 Consuma de forma consciente.</h4>
-</body>
-</html>
